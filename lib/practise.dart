beb1de11cafe9946cbaff7e49289d3cf8921489d
vansh vishwakarma <vanshvishwakarma1514@gmail.com>
2026-05-14 20:37:20 +0530

    practise

---
+++ lib/practise.dart
@@ -0,0 +1,11 @@
+import  'dart:io';
+void main()
+{
+  print("hello");
+  //
+  // stdout.write("enter  your name ");
+
+  stdin.readLineSync();
+  var name = stdin.readLineSync();
+  print("your name , +name");
+}
