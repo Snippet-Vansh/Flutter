62022dbe033754c886d3a10b00002c5cdb0db0f2
vansh vishwakarma <vanshvishwakarma1514@gmail.com>
2026-05-28 14:04:55 +0530

    custom widget

---
+++ lib/CallBack.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(FlutterApp());
+}
+
+class FlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "FlutterApp",
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: DashBoardScreen(),
+    );
+  }
+}
+
+class DashBoardScreen extends StatefulWidget {
+  @override
+  State<DashBoardScreen> createState() => _DashBoardScreenState();
+}
+
+class _DashBoardScreenState extends State<DashBoardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightGreen,
+          title: Text('Call Back'),
+        ),
+        body: ElevatedButton(
+            child: Text('click'),
+             onPressed: () {
+              print('Clicked!!!1');
+    },
+
+        ),
+
+
+    );
+  }
+}
