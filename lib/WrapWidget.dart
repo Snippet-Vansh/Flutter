4525fd9ec64f98ae685e3db713e537f3c0f7d2b5
Ezzzz-Vansh🦖 <vanshvishwakarma1514@gmail.com>
2026-06-01 15:54:34 +0530

    Add files via upload

---
+++ lib/WrapWidget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Boilerplate',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Boilerplate'),
+      ),
+      body:
+      Row(
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.orange,
+          ),
+
+
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.orange,
+          ),
+
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.orange,
+          ),
+
+
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.orange,
+          ),
+      ],
+      ),
+
+
+    );
+  }
+}
