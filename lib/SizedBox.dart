55e5278a0116128e3151894070180ea35094bdd0
Ezzzz-Vansh🦖 <vanshvishwakarma1514@gmail.com>
2026-06-01 15:59:41 +0530

    Add files via upload

---
+++ lib/SizedBox.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Boilerplate',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Boilerplate'),
+      ),
+      body: Center(
+        //Si
+        child: SizedBox.square(
+         dimension: 200,
+          child: ElevatedButton(onPressed: (){
+
+          }, child: Text('click')),
+        ),
+      )
+    );
+  }
+}
