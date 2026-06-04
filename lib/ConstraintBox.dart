11bd4b64dc98bef75fdedcd055b1cf4329a04472
Ezzzz-Vansh🦖 <vanshvishwakarma1514@gmail.com>
2026-06-04 10:41:14 +0530

    Add files via upload

---
+++ lib/ConstraintBox.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Boilerplate',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stateful'),
+      ),
+      body: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: 200,
+            
+          ),
+          child: Text('Hello world',style: TextStyle(fontSize: 21),))
+    );
+  }
+}
