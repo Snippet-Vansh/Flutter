29dd457b8238f044426de06d50bba7976f492bb9
Ezzzz-Vansh🦖 <vanshvishwakarma1514@gmail.com>
2026-06-15 10:06:56 +0530

    Add files via upload

---
+++ lib/StackWidget.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.orange,
+            ),
+            Positioned(
+              left: 20,
+              right: 20,
+              top: 20,
+              bottom: 20,
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.green,
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
